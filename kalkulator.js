0ed5f6e1aac10a0900edb46bbda9da0fa9687cfa
fadlimuh <kudhen123@gmail.com>
2024-03-18 11:20:19 +0700

    V1.0

---
+++ kalkulator.js
@@ -0,0 +1,13 @@
+function hitung(operator) {
+  const angka1 = parseInt(document.getElementById('angka1').value);
+  const angka2 = parseInt(document.getElementById('angka2').value);
+  let hasil;
+
+  switch (operator) {
+      case '+':
+          hasil = angka1 + angka2;
+          break;
+  }
+
+  document.getElementById('hasil').innerHTML = hasil;
+}
